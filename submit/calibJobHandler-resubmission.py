e308df8efbcdacab9f8c3c527a454ea73e405e88
lpernie <>
2013-02-11 10:30:26 +0000

    onlyFinalHadd=False at the end

--- submit/calibJobHandler-resubmission.py
+++ submit/calibJobHandler-resubmission.py
@@ -39,7 +39,11 @@ for iters in range(Iter_toResub, nIterations):
           env_script_n.close()
           fill_log_n = logPath + "/fillEpsilonPlot_iter_" + str(iters) + "_job_" + str(ijob) + ".log"
           fill_src_n = srcPath + "/Fill/submit_iter_"     + str(iters) + "_job_" + str(ijob) + ".sh"
-          submit_s = "bsub -q " + queue + " -o " + fill_log_n + " " + fill_src_n
+          if not(Silent): 
+               submit_s = "bsub -q " + queue + " -o " + fill_log_n + " " + fill_src_n
+          else:
+               submit_s = "bsub -q " + queue + " -o /dev/null -e /dev/null " + fill_src_n
+
           print '\n[job #' + str(ijob) + '] :: ' + submit_s
 
           # actually submitting filling tasks
@@ -555,5 +559,6 @@ for iters in range(Iter_toResub, nIterations):
 
    print "Done with iteration " + str(iters)
    onlyFIT='False'
+   onlyFinalHadd='False'
 
 print "---THE END---"
