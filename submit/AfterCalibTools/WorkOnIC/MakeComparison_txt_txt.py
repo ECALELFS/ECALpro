6954e801b8b43579d10624322b6f17d50cf3281d
lpernie <>
2013-04-03 12:32:21 +0000

    txt dii xtal + Comparison eta<1

--- submit/AfterCalibTools/WorkOnIC/MakeComparison_txt_txt.py
+++ submit/AfterCalibTools/WorkOnIC/MakeComparison_txt_txt.py
@@ -20,19 +20,19 @@ def usage():
     print "Usage: -----> python MakeComparison_txt_txt.py fileMINE.txt fileCALT.txt output"
 #python MakeComparison_txt_txt.py /afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/energy-calibration-repository/piZero/lpernie/IC_2012C_MVAcorrResid_13Iter_MediatedGlobally.txt
 #/afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/energy-calibration-repository/piZero/2012C_NewTags_V2GOOD/interCalibConstants.combinedPi0EtaAllPeriod.EBandEE_2012C_newLaserTag_run197770-203755_GOOD.txt
-#Compare2012C/2012C_GlobalMY_Vs_Caltech.root
+#Compare2012C 2012C_GlobalMY_Vs_Caltech.root
 
 #python MakeComparison_txt_txt.py /afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/energy-calibration-repository/piZero/lpernie/IC_2012B_MVAcorrResid_11Iter_MediatedGlobally.txt
 #/afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/energy-calibration-repository/piZero/2012B_NewTags_V2GOOD/interCalibConstants.2012B.EBandEE.GOOD.txt
-#Compare2012B/2012B_GlobalMY_Vs_Caltech.root
+#Compare2012B 2012B_GlobalMY_Vs_Caltech.root
 
 #python MakeComparison_txt_txt.py /afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/energy-calibration-repository/piZero/lpernie/IC_2012D_MVAcorrResid_13Iter_MediatedGlobally.txt
 #/afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/energy-calibration-repository/piZero/2012D_NewTags_V2GOOD/interCalibConstants.combined_2012D_EBandEE_GOOD.txt
-#Compare2012D/2012D_GlobalMY_Vs_Caltech.root
+#Compare2012D 2012D_GlobalMY_Vs_Caltech.root
 
 ###START
 print "-----------------START-------------------"
-if (len(sys.argv) != 4):
+if (len(sys.argv) != 5):
     usage()
     sys.exit(1)
 try:
@@ -72,10 +72,13 @@ for nXtal in range(len(file1_v)):
     else:
        IC_EE_1.append([List1[0],List1[1],List1[2],List1[3]]) #iX iY iZ IC
        IC_EE_2.append([List2[0],List2[1],List2[2],List2[3]])
-       #print 'EE: ' + str(nXtal-61200) + ': ' + str(IC_EE_1[nXtal-61200][0]) + ' ' + str(IC_EE_1[nXtal-61200][1]) + ' ' + str(IC_EE_1[nXtal-61200][2]) + ' ' + str(IC_EE_1[nXtal-61200][3])
+       #print 'EE1: ' + str(nXtal-61200) + ': ' + str(IC_EE_1[nXtal-61200][0]) + ' ' + str(IC_EE_1[nXtal-61200][1]) + ' ' + str(IC_EE_1[nXtal-61200][2]) + ' ' + str(IC_EE_1[nXtal-61200][3])
+       #print 'EE2: ' + str(nXtal-61200) + ': ' + str(IC_EE_2[nXtal-61200][0]) + ' ' + str(IC_EE_2[nXtal-61200][1]) + ' ' + str(IC_EE_2[nXtal-61200][2]) + ' ' + str(IC_EE_2[nXtal-61200][3])
 #Out Put
-f = TFile(str(sys.argv[3]), 'recreate')
+f = TFile(str(sys.argv[3]) + "/" + str(sys.argv[4]) , 'recreate')
+text_file = open(str(sys.argv[3]) + "/Different_xTal.txt", "w")
 Ratio1D_EB = TH1F("Ratio1D_EB", "Ratio IC EB", 100, 0.95, 1.05)
+Ratio1D_EB_eta1 = TH1F("Ratio1D_EB_eta1", "Ratio IC EB", 100, 0.95, 1.05)
 Ratio1D_EEm = TH1F("Ratio1D_EEm", "Ratio IC EEm", 100, 0.7, 1.3)
 Ratio1D_EEp = TH1F("Ratio1D_EEp", "Ratio IC EEp", 100, 0.7, 1.3)
 Ratio_EBMap = TH2F("Ratio_EBMap","Ratio EB Map #phi on x, #eta on y", 360,0.5,360.5,171,-85.5,85.5)
@@ -136,6 +139,8 @@ for find_EB1 in range(len(IC_EB_1)):
            if(float(IC_EB_1[find_EB1][2])!=-1. and float(IC_EB_2[find_EB2][2])!=-1. ):
               print 'Eta: ' + str(IC_EB_1[find_EB1][0]) + ', Phi = ' + str(IC_EB_1[find_EB1][1]) + ': ' + str(IC_EB_1[find_EB1][2]) + ' vs ' + str(IC_EB_2[find_EB2][2])
               Ratio1D_EB.Fill(float(IC_EB_1[find_EB1][2])/float(IC_EB_2[find_EB2][2]))
+              if( abs(int(IC_EB_1[find_EB1][0]))<45. ): Ratio1D_EB_eta1.Fill(float(IC_EB_1[find_EB1][2])/float(IC_EB_2[find_EB2][2]))
+              if( abs(float(IC_EB_1[find_EB1][2])/float(IC_EB_2[find_EB2][2]))>1.02 ): text_file.write( "iEta: " + str(int(IC_EB_1[find_EB1][0])+86) + " iPhi " + (str(IC_EB_1[find_EB1][1])) + " IC1/IC2: " + str(abs(float(IC_EB_1[find_EB1][2])/float(IC_EB_2[find_EB2][2]))) + "\n")
               Ratio_EBMap.SetBinContent(int(IC_EB_1[find_EB1][1]), int(IC_EB_1[find_EB1][0])+86, float(IC_EB_1[find_EB1][2])/float(IC_EB_2[find_EB2][2]))
               cal_EBMap1.SetBinContent(int(IC_EB_1[find_EB1][0])+86, int(IC_EB_1[find_EB1][1]), float(IC_EB_1[find_EB1][2]))
               cal_EBMap2.SetBinContent(int(IC_EB_2[find_EB2][0])+86, int(IC_EB_2[find_EB2][1]), float(IC_EB_2[find_EB2][2]))
@@ -149,6 +154,8 @@ for find_EB1 in range(len(IC_EB_1)):
         print 'WARNING, an xtal in file1 is not in file2!!!'
 #-----------------------
 #Compare EE
+IC1_bad=0
+IC2_bad=0
 print 'and then with EE (' + str(len(IC_EE_1)) + ' xtal)'
 if not(len(IC_EE_1)==len(IC_EE_1)):
    print 'WARNING, EE1 != EE2'
@@ -157,38 +164,48 @@ for find_EE1 in range(len(IC_EE_1)):
     finded=False
     for find_EE2 in range(len(IC_EE_2)):
         if(IC_EE_1[find_EE1][0]==IC_EE_2[find_EE2][0] and IC_EE_1[find_EE1][1]==IC_EE_2[find_EE2][1] and IC_EE_1[find_EE1][2]==IC_EE_2[find_EE2][2]):
-           #print 'iX: ' + str(IC_EE_1[find_EE1][0]) + ', iY = ' + str(IC_EE_1[find_EE1][1]) + ', iZ: ' + str(IC_EE_1[find_EE1][2]) + ' | iX: ' + str(IC_EE_2[find_EE2][0]) + ', iY = ' + str(IC_EE_2[find_EE2][1])+ ', iZ: ' + str(IC_EE_2[find_EE2][2])
            s.iX=int(IC_EE_1[find_EE1][0]); s.iY=int(IC_EE_1[find_EE1][1]); s.iZ=int(IC_EE_1[find_EE1][2])
            s.iC_1=float(IC_EE_1[find_EE1][3]); s.iC_2=float(IC_EE_2[find_EE2][3])
            mytreeEE.Fill()
            if(float(IC_EE_1[find_EE1][3])!=-1. and float(IC_EE_2[find_EE2][3])!=-1. ):
-              print 'iX: ' + str(IC_EE_1[find_EE1][0]) + ', iY: ' + str(IC_EE_1[find_EE1][1]) + ', iZ: ' + str(IC_EE_1[find_EE1][2]) + ': ' + str(IC_EE_1[find_EE1][3]) + ' vs ' + str(IC_EE_2[find_EE2][3])
+              #print 'iX: ' + str(IC_EE_1[find_EE1][0]) + ', iY: ' + str(IC_EE_1[find_EE1][1]) + ', iZ: ' + str(IC_EE_1[find_EE1][2]) + ': ' + str(IC_EE_1[find_EE1][3]) + ' vs ' + str(IC_EE_2[find_EE2][3])
               if(int(IC_EE_1[find_EE1][2])==-1):
                  Ratio1D_EEm.Fill(float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))
                  Ratio_EEmMap.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))
                  cal_EEmMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), float(IC_EE_1[find_EE1][3]))
                  cal_EEmMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), float(IC_EE_2[find_EE2][3]))
+                 if(abs(float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))>1.7): text_file.write( "iX: " + str(IC_EE_1[find_EE1][0] + " iY " + (IC_EE_1[find_EE1][1]) + " iZ=-1 IC1/IC2: " + str(float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))) + "\n")
               if(int(IC_EE_1[find_EE1][2])==1):
                  Ratio1D_EEp.Fill(float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))
                  Ratio_EEpMap.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))
                  cal_EEpMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), float(IC_EE_1[find_EE1][3]))
                  cal_EEpMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), float(IC_EE_2[find_EE2][3]))
+                 if(abs(float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))>1.7): text_file.write( "iX: " + str(IC_EE_1[find_EE1][0] + " iY " + (IC_EE_1[find_EE1][1]) + " iZ=1 IC1/IC2: " + str(float(IC_EE_1[find_EE1][3])/float(IC_EE_2[find_EE2][3]))) + "\n")
            else:
               if(int(IC_EE_1[find_EE1][2])==1): 
-                 cal_EEpMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), 1.)
-                 cal_EEpMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), 1.)
+                 if(float(IC_EE_1[find_EE1][3])!=-1.): cal_EEpMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), float(IC_EE_1[find_EE1][3]))
+                 else:                                 cal_EEpMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), 1.)
+                 if(float(IC_EE_2[find_EE1][3])!=-1.): cal_EEpMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), float(IC_EE_2[find_EE1][3]))
+                 else:                                 cal_EEpMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), 1.)
               if(int(IC_EE_1[find_EE1][2])==-1):
-                 cal_EEmMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), 1.)
-                 cal_EEmMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), 1.)
-              print 'No: ' + str(IC_EE_1[find_EE1][0]) + ' ' + str(IC_EE_1[find_EE1][1]) + '' + str(IC_EE_1[find_EE1][2]) + ' ' + str(IC_EE_2[find_EE2][2])
+                 if(float(IC_EE_1[find_EE1][3])!=-1.): cal_EEmMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), float(IC_EE_1[find_EE1][3]))
+                 else:                                 cal_EEmMap1.SetBinContent(int(IC_EE_1[find_EE1][0]), int(IC_EE_1[find_EE1][1]), 1.)
+                 if(float(IC_EE_2[find_EE1][3])!=-1.): cal_EEmMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), float(IC_EE_2[find_EE1][3]))
+                 else                                : cal_EEmMap2.SetBinContent(int(IC_EE_2[find_EE2][0]), int(IC_EE_2[find_EE2][1]), 1.)
+              #print 'No: ' + str(IC_EE_1[find_EE1][0]) + ' ' + str(IC_EE_1[find_EE1][1]) + '' + str(IC_EE_1[find_EE1][2]) + ' ' + str(IC_EE_2[find_EE2][2])
+              if(float(IC_EE_1[find_EE1][3])==-1.): IC1_bad=IC1_bad+1
+              if(float(IC_EE_2[find_EE1][3])==-1.): IC2_bad=IC2_bad+1
            finded=True
            break
     if not(finded):
         print 'WARNING, an xtal in file1 is not in file2!!!'
 print 'Now saving files.'
+print 'IC1 has no: ' + str(IC1_bad) + " xtal"
+print 'IC2 has no: ' + str(IC2_bad) + " xtal"
 
 print "------------------END--------------------"
 #Final Writing
 f.cd()
 f.Write()
+text_file.close()
 f.Close()
