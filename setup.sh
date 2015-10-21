7f86495b3531d07dc101169ab0ae10a69c4b1436
Shervin <shervin@cern.ch>
2015-10-21 15:52:52 +0200

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RELEASE=$1
+case $RELEASE in
+	CMSSW_7_4_15)
+		;;
+	*)
+		echo "[ERROR] Release not supported" >> /dev/stderr
+		exit 1
+		;;
+esac
+
+scram project $RELEASE
+cd $RELEASE/src
+cmsenv
+git cms-init
+git clone git@github.com:ECALELFS/ECALpro.git CalibCode
