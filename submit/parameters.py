d8364fb3ffb475eb6864286766e77e45dacbc56a
Marco Cipriani <cippy91@gmail.com>
2016-05-24 23:26:01 +0200

    most recent version of parameters.py to run on data

--- submit/parameters.py
+++ submit/parameters.py
@@ -25,15 +25,15 @@ myeoscmd = '/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.sele
 myeosls = myeoscmd + 'ls '  #to avoid use of cmsLs that is deprecated since January 2016   
 myeoslsl = myeosls + '-l '
 myeosmkdir = myeoscmd + 'mkdir '
-myeosstage = myeoscmd + 'cp '  
-#myeosstage = 'cmsStage -f '
+#myeosstage = myeoscmd + 'cp '  
+myeosstage = 'cmsStage -f '
 # I called it myeosstage instead of myeoscp to remember that it substitutes cmsStage command
 # as a convention, when adding commands like: command = myeoscmd + "some_option ", just leave a space AFTER the some_option, not before
 # note that code used cmsStage -f, but eos cp doesn't support -f option
 # also, code will copy *.root files from /tmp/ (where they are initially created) to eosPath, but eosPath must be preceeded by "root://eoscms/eos/cms" to have eos cp
 # work as expected. So the destination will be root://eoscms/eos/cms/store/group/dpg_ecal/alca_ecalcalib/piZero2016/mciprian/... . For this reason, we define here
-myPrefixToEosPath = 'root://eoscms//eos/cms'
-#myPrefixToEosPath = ''
+#myPrefixToEosPath = 'root://eoscms//eos/cms'
+myPrefixToEosPath = ''
 # will modify calibJobHandler.py with this prefix to destination
 #
 # end of my additions
@@ -52,33 +52,22 @@ if(isCRAB):
        voGroup     = "itcms"
        storageSite = "T2_IT_Rome"
        outLFN      = "/store/user/mciprian/piZero2016/"
-#adding following line to run on T2 without using crab
-if(isOtherT2):
-   eosPath = '/pnfs/roma1.infn.it/data/cms/store/user/mciprian/piZero2016/'
-   voGroup     = "itcms"
-   storageSite = "T2_IT_Rome"
-   outLFN      = "/store/user/mciprian/piZero2016/"
-# end of my personal addition
 #MC and Selection Optimization
 isMC = False
 MakeNtuple4optimization = False
 #InputList and Folder name
 inputlist_n      = 'InputList/2016B_All.list' # list of input files
-dirname          = 'pi0data_2016B'
+dirname          = 'pi0data_2016B_All'
 Silent           = False                 # True->Fill modules is silent; False->Fill modules has a standard output
 #TAG, QUEUE and ITERS
-NameTag          = 'pi0data_2016B'                   # Tag to the names to avoid overlap
+NameTag          = 'pi0data_2016B_All'                   # Tag to the names to avoid overlap
 queueForDaemon   = 'cmscaf1nw'          # Option suggested: 2nw/2nd, 1nw/1nd, cmscaf1nw/cmscaf1nd... even cmscaf2nw
 queue            = 'cmscaf1nd'
-#adding following lines to run on T2 Rome
-if(isOtherT2):
-   queueForDaemon   = 'cmscan'
-   queue            = 'cmscan'
-nIterations      = 1
+nIterations      = 14
 #N files
 ijobmax          = 5                     # 5 number of files per job
 nHadd            = 35                    # 35 number of files per hadd
-fastHadd         = True                  # From 7_4_X we can use this faster mathod. But files have to be copied on /tmp/ to be converted in .db
+fastHadd         = False                  # From 7_4_X we can use this faster mathod. But files have to be copied on /tmp/ to be converted in .db
 if( isCRAB and isOtherT2 ):
    fastHadd      = False                 # No fastHadd on a different T2
 nFit             = 2000                  # number of fits done in parallel
