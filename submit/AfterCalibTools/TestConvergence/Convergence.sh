192755e95be246a79067690928f9f4a8aba08185
Marco Cipriani <cippy91@gmail.com>
2016-12-30 11:29:55 +0100

    store plots in 'extension' folder when extensions are used

--- submit/AfterCalibTools/TestConvergence/Convergence.sh
+++ submit/AfterCalibTools/TestConvergence/Convergence.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 eosPath="/store/group/dpg_ecal/alca_ecalcalib/piZero2016/mciprian/" 
-dirName="AlcaP0_2016_json3p99fb_weight"                            # dirname (see CalibCode/submit/parameters.py)  
-iter_number="4"                                                          # number n of iterations (iter_0 to iter_{n-1})
-tagName="AlcaP0_2016_json3p99fb_weight_"                           # TagName (see CalibCode/submit/parameters.py)  
+dirName="AlcaP0_2016_json3p99fb_weight_8iter_noCC"                            # dirname (see CalibCode/submit/parameters.py)  
+iter_number="8"                                                          # number n of iterations (iter_0 to iter_{n-1})
+tagName="AlcaP0_2016_json3p99fb_weight_8iter_noCC_"                           # TagName (see CalibCode/submit/parameters.py)  
 
 # will copy output here, if directory exists
 wwwTargetDir="/afs/cern.ch/user/m/mciprian/www/pi0calib/ICplot/${dirName}/TestConvergence/"               
@@ -11,8 +11,14 @@ wwwTargetDir="/afs/cern.ch/user/m/mciprian/www/pi0calib/ICplot/${dirName}/TestCo
 nJump=1
 # leave extension as "noExtension" in you don't need to add additional steps that start from the one above
 # format is newDirName,newIterNumber,newTagName
-#extension="noExtension"
-extension="AlcaP0_2016_json3p99fb_weight_ext,4,AlcaP0_2016_json3p99fb_weight_ext_"
+extension="noExtension"
+#extension="AlcaP0_2016_json3p99fb_weight_ext,4,AlcaP0_2016_json3p99fb_weight_ext_"
+
+if [ "${extension}" != "noExtension" ]
+then
+    echo "Extension added: will store plots in --> ${wwwTargetDir}extension/ (if it exists)"
+    wwwTargetDir=${wwwTargetDir}extension/
+fi
 
 #compile Convergence.C
 g++ -Wall -pedantic -lm -o Convergence Convergence.C `$ROOTSYS/bin/root-config --cflags --libs` 
