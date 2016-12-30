b54c2509cb0df303a36328d11a7550f65a5c4603
Marco Cipriani <cippy91@gmail.com>
2016-12-30 11:22:40 +0100

    added folder to separately store convergence plots when extensions are added

--- submit/AfterCalibTools/PlotMaker/makeDirWWW.sh
+++ submit/AfterCalibTools/PlotMaker/makeDirWWW.sh
@@ -4,7 +4,7 @@ currentPath="$PWD"
 
 wwwPath="/afs/cern.ch/user/m/mciprian/www/"  # your afs path to directory associated to your website
 basePath="${wwwPath}pi0calib/ICplot/"        # here you will create the dirname folder (you can choose the name you want, add more folders or simply use one)
-baseDir="AlcaP0_2016_json3p99fb_weight_8iter"       # dirname (could use any name, but better to stick with dirname in parameters.py)
+baseDir="AlcaP0_2016_json3p99fb_weight_8iter_noCC"       # dirname (could use any name, but better to stick with dirname in parameters.py)
 iter_ini=0  # first iteration to use (in general it would be 0)                                                                                   
 iter_fin=7  # last iteration to use: it is included in sequence below (if you did n iterations, this should be n-1)                                                  
 
@@ -17,7 +17,7 @@ do
     mkdir -p ${basePath}${baseDir}/iter_${i}/fitResPlots/Endcap
 done
 
-mkdir -p ${basePath}${baseDir}/TestConvergence
+mkdir -p ${basePath}${baseDir}/TestConvergence/extension   # use extension directory to store plots when you add extensions to a set of ICs
 
 cd $wwwPath
 ./copyphp.sh   # you should already have this script to be able to see files in your website
