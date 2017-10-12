2c3f072d989a281575d3ead4805d3bc463db73da
Marco Cipriani <cippy91@gmail.com>
2017-10-12 23:13:42 +0200

    for very first iteration use different ranges (unless it is an extension)

--- submit/AfterCalibTools/PlotMaker/2DmapMaker/calibAnaEcalEB.C
+++ submit/AfterCalibTools/PlotMaker/2DmapMaker/calibAnaEcalEB.C
@@ -69,7 +69,7 @@ void calibAnaEcalEB::setHistograms() {
   th2dMinZaxisVector.push_back(0.0);
   th2dMinZaxisVector.push_back(0.0005);//0.0                                                                 
   if (Pi0orEta == "Pi0") {
-    if (this->getIterNumber() == "iter_0") th2dMinZaxisVector.push_back(0.120);
+    if (this->getIterNumber() == "iter_0" && this->getDirName().find("_ext") == string::npos) th2dMinZaxisVector.push_back(0.120);
     else                            th2dMinZaxisVector.push_back(0.130);
     th2dMinZaxisVector.push_back(0.005);
   } else {
@@ -89,7 +89,7 @@ void calibAnaEcalEB::set2DmapMaxZaxisVector() {
   th2dMaxZaxisVector.push_back(10e9); // when this value is very large (bigger than the default) use the default to plot axis                  
   th2dMaxZaxisVector.push_back(0.0125);//0.02                                                  
   if (Pi0orEta == "Pi0") {
-    if (this->getIterNumber() == "iter_0") th2dMaxZaxisVector.push_back(0.130);
+    if (this->getIterNumber() == "iter_0" && this->getDirName().find("_ext") == string::npos) th2dMaxZaxisVector.push_back(0.130);
     else                            th2dMaxZaxisVector.push_back(0.140);
     th2dMaxZaxisVector.push_back(0.015);
   } else {
@@ -202,7 +202,7 @@ void calibAnaEcalEB::Loop()
   mean_iphiProfile->GetXaxis()->SetTitleOffset(0.7);
   mean_iphiProfile->GetYaxis()->SetTitle("mean [GeV]");
   if (Pi0orEta == "Pi0") {
-    if (this->getIterNumber() == "iter_0") mean_iphiProfile->GetYaxis()->SetRangeUser(0.12,0.13);
+    if (this->getIterNumber() == "iter_0" && this->getDirName().find("_ext") == string::npos) mean_iphiProfile->GetYaxis()->SetRangeUser(0.12,0.13);
     else                                   mean_iphiProfile->GetYaxis()->SetRangeUser(0.13,0.14);
   } else mean_iphiProfile->GetYaxis()->SetRangeUser(0.5,0.6);
   mean_iphiProfile->GetYaxis()->SetTitleSize(0.055);
