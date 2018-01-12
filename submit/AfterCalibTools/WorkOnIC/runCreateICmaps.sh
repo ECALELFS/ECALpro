ed1601fb5489c0fe669f2330d8c650ecc3789171
Marco Cipriani <cippy91@gmail.com>
2018-01-12 23:23:35 +0100

    committing shell script: wrapper to use createICmaps.sh

---
+++ submit/AfterCalibTools/WorkOnIC/runCreateICmaps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# source createICmaps.sh AlCaP0_Run2017B_3July_upToRun297723 0 2017 0
+# source createICmaps.sh AlCaP0_Run2017B_3July_upToRun297723 1 2017 0
+# source createICmaps.sh AlCaP0_Run2017B_3July_upToRun297723_ext1 3 2017 0
+
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017 0 2017 0
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017 1 2017 0
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017 2 2017 1
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017 3 2017 1
+
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017_2012regression 0 2017 1
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017_2012regression_ext1_fromIter_0 0 2017 0
+
+#source createICmaps.sh AlcaP0_2016H_mar2017newCond_reg2012 1 2016 1
+#source createICmaps.sh AlCaP0_IC2017_upTo21September2017_2012regression_v2 4 2017 1
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017_2012regression_v2 2 2017 1
+# source createICmaps.sh AlCaP0_IC2017_upTo21September2017_2012regression_v2 3 2017 1
+
+#source createICmaps.sh AlcaP0_Run2016G_sel16_reg12 4 2017 0
+#source createICmaps.sh AlcaP0_Run2016G_sel17optim_reg12 4 2017 0
+#source createICmaps.sh AlcaP0_Run2016G_sel17optim_reg12_ext1_fromIter4 4 2017 1
+#source createICmaps.sh AlcaP0_Run2016G_sel17optim_reg12_v2 4 2017 1
+#source createICmaps.sh AlCaP0_DAS2017_Run2017C_run300226_300401_2017Regression 0 2017 1 zhicaiz
+
+source createICmaps.sh AlCaP0_Run2017_DE_run304366_v2 0 2017 1 mciprian
+#source createICmaps.sh AlCaP0_Run2017_DE_run304366_v2 6 2017 0 mciprian
+#source createICmaps.sh AlCaP0_Run2017_DE_run304366_v2_ext1 6 2017 0 mciprian
+#source createICmaps.sh testDataCalib_newClustering 0 2017 1 mciprian
