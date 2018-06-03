c671c3ce5e98b468dd4db362022f72bb368dba54
Marco Cipriani <cippy91@gmail.com>
2018-06-03 13:08:20 +0200

    fix to use GetByToken (the fix was necessary in release > 10X, but apparently it was supposed to be needed also in 9X, so I am backporting it)

--- FillEpsilonPlot/plugins/DummyRechitDigis.cc
+++ FillEpsilonPlot/plugins/DummyRechitDigis.cc
@@ -59,6 +59,11 @@ private:
   const std::string barrelDigiCollection_;
   const std::string endcapDigiCollection_;
   const bool doDigi_;
+  edm::EDGetTokenT<EcalRecHitCollection>  barrelRecHit_pi0_token_;
+  edm::EDGetTokenT<EcalRecHitCollection>  endcapRecHit_pi0_token_;
+  edm::EDGetTokenT<EBDigiCollection>  barrelDigis_pi0_token_;
+  edm::EDGetTokenT<EEDigiCollection>  endcapDigis_pi0_token_;
+
 };
 
 DummyRechitDigis::DummyRechitDigis(const edm::ParameterSet& iConfig):
@@ -75,11 +80,15 @@ DummyRechitDigis::DummyRechitDigis(const edm::ParameterSet& iConfig):
   if(doDigi_) { 
     produces< EBDigiCollection >(barrelDigiCollection_);
     produces< EEDigiCollection >(endcapDigiCollection_);
+    barrelDigis_pi0_token_ = consumes<EBDigiCollection> (tag_barrelDigiProducer_);
+    endcapDigis_pi0_token_ = consumes<EEDigiCollection> (tag_endcapDigiProducer_);
   }
   else {
     produces< EcalRecHitCollection >(barrelRecHitCollection_);
     produces< EcalRecHitCollection >(endcapRecHitCollection_);
-  }
+    barrelRecHit_pi0_token_ = consumes<EcalRecHitCollection> (tag_barrelHitProducer_);
+    endcapRecHit_pi0_token_ = consumes<EcalRecHitCollection> (tag_endcapHitProducer_);
+   }
 }
 
 DummyRechitDigis::~DummyRechitDigis(){ }
@@ -110,11 +119,14 @@ void DummyRechitDigis::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
 
    // fake digis
    // handle to try to fill
-   Handle<EBDigiCollection> digisEBHandle;
-   Handle<EEDigiCollection> digisEEHandle;
+   edm::Handle<EBDigiCollection> digisEBHandle;
+   edm::Handle<EEDigiCollection> digisEEHandle;
    // dummy collection to Put()
    std::unique_ptr<EBDigiCollection> outputEBDigiCollection( new EBDigiCollection );
    std::unique_ptr<EEDigiCollection> outputEEDigiCollection( new EEDigiCollection );
+
+   std::unique_ptr< EcalRecHitCollection > outputEBRecHitCollection( new EcalRecHitCollection);
+   std::unique_ptr< EcalRecHitCollection > outputEERecHitCollection( new EcalRecHitCollection);
 
    //Digi zero_digi;
    EBDigiCollection ebfakecol;
@@ -129,56 +141,49 @@ void DummyRechitDigis::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
    *fakeEEDigiCollection = eefakecol;
 
 
-   if(!doDigi_) { 
-     bool foundEBRechit = true;
-     bool foundEERechit = true;
+   if(doDigi_) { 
 
-     // if you dont find the barrel rechits youre looking for, put in a fake one
-     try { 
-       iEvent.getByLabel(tag_barrelHitProducer_, barrelRecHitsHandle);
-       *rechits_eb = *(barrelRecHitsHandle.product());       
-     }
-     catch(cms::Exception& ex) { foundEBRechit = rechits_eb->size() > 0;}     
-     // if you found the collection put it back into the event
-     iEvent.put( foundEBRechit ? std::move(rechits_eb) : std::move(rechits_temp), barrelRecHitCollection_);
-     
-     // if you dont find the endcap rechits youre looking for, put in a fake one
-     try {
-       iEvent.getByLabel(tag_endcapHitProducer_, endcapRecHitsHandle);
-       *rechits_ee = *(endcapRecHitsHandle.product());
-     } 
-     catch (cms::Exception& ex){ foundEERechit = rechits_ee->size() > 0;
-     } 
-
-     iEvent.put( foundEERechit ? std::move(rechits_ee) : std::move(rechits_temp2), endcapRecHitCollection_);
-   } // end dummy rechits
-   
-   // Build fake digi collections
-   else {    
-     bool foundEBDigi = true;
-     bool foundEEDigi = true;
-
-     try { // barrel digis
-       iEvent.getByLabel(tag_barrelDigiProducer_, digisEBHandle);
+     iEvent.getByToken(barrelDigis_pi0_token_, digisEBHandle);
+     if (digisEBHandle.isValid()) {
        *outputEBDigiCollection = *(digisEBHandle.product());
+       // insert the EB collection
+       iEvent.put(std::move(outputEBDigiCollection), barrelDigiCollection_);
+     } else {
+       iEvent.put(std::move(fakeEBDigiCollection), barrelDigiCollection_);
+
      }
-     catch (cms::Exception& ex) {
-       foundEBDigi = outputEBDigiCollection->size() > 0;
-     }     
 
-     // insert the EB collection
-     iEvent.put(foundEBDigi ? std::move(outputEBDigiCollection) : std::move(fakeEBDigiCollection), barrelDigiCollection_);
-
-     try { // endcap digis
-       iEvent.getByLabel(tag_endcapDigiProducer_, digisEEHandle);
+     iEvent.getByToken(endcapDigis_pi0_token_, digisEEHandle);
+     if (digisEEHandle.isValid()) {
        *outputEEDigiCollection = *(digisEEHandle.product());
+       iEvent.put(std::move(outputEEDigiCollection), endcapDigiCollection_);
+     } else {
+       iEvent.put(std::move(fakeEEDigiCollection), endcapDigiCollection_);
      }
-     catch (cms::Exception& ex) {
-       foundEEDigi = outputEEDigiCollection->size() > 0;
-     }
-     //     std::cout << "Putting Real EE collection?  " << foundEEDigi <<  std::endl;
-     iEvent.put(foundEEDigi ? std::move(outputEEDigiCollection) : std::move(fakeEEDigiCollection), endcapDigiCollection_);
      
+   } // end dummy digis
+   
+   // Build fake rechits collections
+   else {    
+
+     iEvent.getByToken(barrelRecHit_pi0_token_, barrelRecHitsHandle);
+     // if you dont find the barrel rechits youre looking for, put in a fake one
+     if (barrelRecHitsHandle.isValid()) {
+       *rechits_eb = *(barrelRecHitsHandle.product());
+       iEvent.put(std::move(rechits_eb), barrelRecHitCollection_);
+     } else {
+       iEvent.put(std::move(rechits_temp), barrelRecHitCollection_);
+     }
+     
+     iEvent.getByToken(endcapRecHit_pi0_token_, endcapRecHitsHandle);
+     // if you dont find the endcap rechits youre looking for, put in a fake one
+     if (endcapRecHitsHandle.isValid()) {
+       *rechits_ee = *(endcapRecHitsHandle.product());
+       iEvent.put(std::move(rechits_ee), endcapRecHitCollection_);
+     } else {
+       iEvent.put(std::move(rechits_temp2), endcapRecHitCollection_);
+     }
+
    }
    //std::cout << "-----------End Dummy Rechits ---------- " << std::endl;
 }
