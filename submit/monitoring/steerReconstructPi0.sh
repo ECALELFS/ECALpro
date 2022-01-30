b4bd2cad831203e3c39dc1a891b400f0b0d88456
Shilpi <shilpi.jain@cern.ch>
2022-01-30 01:43:07 +0100

    optional conditions file name

--- submit/monitoring/steerReconstructPi0.sh
+++ submit/monitoring/steerReconstructPi0.sh
@@ -2,7 +2,8 @@
 
 
 inputFiles=('root://cms-xrd-global.cern.ch//store/data/Run2018D/AlCaP0/RAW/v1/000/321/396/00000/248ADA80-FBA1-E811-8742-FA163E7B2F96.root')
-globaltag='105X_dataRun2_v8'
+#globaltag='105X_dataRun2_v8'
+globaltag='121X_mcRun3_2021_realistic_v18'
 jsonFile='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt'
 outputDir='./'
 outputFile='AlCaP0.root'
@@ -10,4 +11,4 @@ conditionsFileName='GTconditions_cff'
 #cd /afs/cern.ch/user/e/ecalgit/pi0Monitoring4Run3/CMSSW_10_6_8/src/CalibCode/submit
 cd /afs/cern.ch/user/e/ecalgit/pi0Monitoring4Run3/CMSSW_12_2_0/src/CalibCode/submit/monitoring/
 eval `scramv1 runtime -sh`
-cmsRun reconstructPi0_template.py inputFiles=${inputFiles} globaltag=${globaltag} jsonFile=${jsonFile} outputDir=${outputDir} outputFile=${outputFile}
+cmsRun reconstructPi0_template.py inputFiles=${inputFiles} globaltag=${globaltag} jsonFile=${jsonFile} outputDir=${outputDir} outputFile=${outputFile} conditionsFileName=${conditionsFileName}
