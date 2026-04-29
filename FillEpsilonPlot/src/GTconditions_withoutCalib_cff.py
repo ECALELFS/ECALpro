8ce7d5f6b88c126782a5029f1864b5e7da8a3863
Thomas Reis <thomas.reis@cern.ch>
2026-04-29 11:40:32 +0200

    Remove config files from src directory. (#48)

--- FillEpsilonPlot/src/GTconditions_withoutCalib_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-GTconditions = cms.VPSet(
-)
--- FillEpsilonPlot/src/cleanedDigiCollectionProducer_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-cleanedEcalDigis = cms.EDFilter("CleanedDigiCollectionProducer",
-    ebDigis = cms.InputTag("ecalDigis","ebDigis"),
-    eeDigis = cms.InputTag("ecalDigis","eeDigis"),
-    cleanedEBDigiCollection = cms.string('ebCleanedDigis'),
-    cleanedEEDigiCollection = cms.string('eeCleanedDigis'),
-    filter = cms.bool( True )
-)
